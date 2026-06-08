23bfff727ff33f22c44c9a1fa6bd5b524bd493d8
ugalvez-glitch <ugalvez@yahoo.com>
2026-06-08 08:50:44 -0700

    Updated for my questions

--- Ulysses.py
+++ Ulysses.py
@@ -1,1 +1,7 @@
+# Chapt. 8 
+# Question 1 - What is the difference between an E-score and Credit Score?
+# Question 2 - Why are E-scores considered a WMD and a Credit Score not?
 
+# Chapt. 10
+# Question 1 - What company targeted users on their platform to increase voting patterns?
+# Question 2 - What presidential candidate hired analysts to increase their odds of winning?
