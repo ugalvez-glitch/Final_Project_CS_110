58f9d68c44f7bd88a93a42ca6f36c89c52980ef1
ugalvez-glitch <ugalvez@yahoo.com>
2026-06-12 12:30:02 -0700

    Update Part_1.py
    Starting for working code

--- Part_1.py
+++ Part_1.py
@@ -1,52 +1,9 @@
 #This is the main file for chapt. 8
-def main():
-	
 	
 #Ulysses questions 
-answer_list_chap_8_question_1 = ["A. An E-score is a score generated based on habits such as where you shop or where you live, a credit score is a score generated based on how much debt you have or how much credit is available to you",
-	"B. An E-score is an expedited score, a credit score is how much credit you have at a store",
-	"C. A credit score is how much money is available to you, an E-score is how financially worthy you are."]
-	answer_list_chap_8_question_2 = ["A. It is not a WMD because E-Scores accurately categorize people based on their habits, which is who they are.", 
-	"B. It is a WMD because E-scores label people based on irrelevant factors. "]
-	print("-------------------")
-	print("These questions are based on chapter 8 in the book")
-	print("What is the difference between an E-Score and Credit Score?")
-	for answer in answer_list_chap_8_question_1:
-		print(answer)
-		
-	print("-------------------")
-	while True:
-		answer = input("What is the correct answer? ").strip().lower()
-		if answer == 'a':
-			print("Correct!")
-			break
-		elif answer == 'b':
-			print("Incorrect")
-		elif answer == 'c':
-			print("Incorrect")
-		else:
-			print("Please type in a letter from the list")
-	
-	print("-------------------")
-	print("Are E-Scores a WMD?")
-	for answer in answer_list_chap_8_question_2:
-		print(answer)
-	print("-------------------")
-	while True:
-		answer = input("What is the correct answer? ").strip().lower()
-		if answer == 'a':
-			print("Incorrect")
-			break
-		elif answer == 'b':
-			print("Correct!")
-			break
-		else:
-			print("Please type in a letter from the list")
-			
-	print("-------------------")
-	
 #Randy's questions 
-	def question_one():
+"""
+def question_one():
     print("Question 1:")
     print("How can the feedback loop created by e-scores contribute to wealth inequality?\n")
     
@@ -122,3 +79,106 @@ print("c.) Created by Iran")
 print("d.) They are the most effective way to anaylyze a persons future finances")
 
 # Anna's questions 
+What might a company use as credit score proxies to decide the structure of a loan or credit card offer they 
+advertise to a user?
+A. User's location
+B. User's online activity
+C. Public government data about the user
+D. All of the above (correct answer)
+
+
+The location of a user's computer is a proxy because it can...
+A. Determine if the user received a degree from a nearby college or university for inference of education level.  
+B. Be matched to the user's zip code for analysis of real estate data and trends. (correct answer)
+C. Show if the user has any previous criminal history for correlation with status of employment.    
+D. Provide access to the user's search history and past purchases to estimate income.
+"""
+
+
+def main():
+	#Ulysses Questions
+	answer_list_chap_8_question_1_Ulysses = ["A. An E-score is a score generated based on habits such as where you shop or where you live, a credit score is a score generated based on how much debt you have or how much credit is available to you",
+	"B. An E-score is an expedited score, a credit score is how much credit you have at a store",
+	"C. A credit score is how much money is available to you, an E-score is how financially worthy you are."]
+	answer_list_chap_8_question_2_Ulysses = ["A. It is not a WMD because E-Scores accurately categorize people based on their habits, which is who they are.", 
+	"B. It is a WMD because E-scores label people based on irrelevant factors. "]
+	print("-------------------")
+	print("These questions are based on chapter 8 in the book")
+	print("-------------------")
+	print("What is the difference between an E-Score and Credit Score?")
+	for answer in answer_list_chap_8_question_1_Ulysses:
+		print(answer)
+	while True:
+		answer = input("What is the correct answer? ").strip().lower()
+		if answer == 'a':
+			print("Correct!")
+			break
+		elif answer == 'b':
+			print("Incorrect")
+		elif answer == 'c':
+			print("Incorrect")
+		else:
+			print("Please type in a letter from the list")
+	
+	print("-------------------")
+	print("Are E-Scores a WMD?")
+	for answer in answer_list_chap_8_question_2_Ulysses:
+		print(answer)
+	print("-------------------")
+	while True:
+		answer = input("What is the correct answer? ").strip().lower()
+		if answer == 'a':
+			print("Incorrect")
+			break
+		elif answer == 'b':
+			print("Correct!")
+			break
+		else:
+			print("Please type in a letter from the list")
+			
+	print("-------------------")
+	
+	#Randy's questions
+	answer_list_chap_8_question_1_Randy = [ "A. It gives everyone equal access to financial opportunities.", "B. It helps people with low scores improve their credit more quickly." ,
+	"C. It can cause people with lower scores to face higher costs and fewer opportunities, making it harder to improve their situation." , "D. It guarantees that all lending decisions are fair and unbiased."]
+	answer_list_chap_8_question_2_Randy = [ "A. They are updated too frequently.", "B. They rely only on credit history.", "C. They are publicly available and routinely reviewed by government agencies.",
+	"D. They are arbitrary, unaccountable, unregulated, and often unfair."]
+	print("How can the feedback loop created by e-scores contribute to wealth inequality?\n")
+	for answer in answer_list_chap_8_question_1_Randy:
+		print(answer)
+	while True:
+		answer = input("What is the correct answer? ").strip().lower()
+		if answer == 'a':
+			print("Incorrect")
+		elif answer == 'b':
+			print("Incorrect")
+		elif answer == 'c':
+			print("Correct!")
+			break
+		elif answer == "d":
+			print("Incorrect")
+		else:
+			print("Please type in a letter from the list")
+	print("-------------------")
+	print("What does Cathy O'Neil identify as a major problem with e-scores?\n")
+	for answer in answer_list_chap_8_question_2_Randy:
+		print(answer)
+	while True:
+		answer = input("What is the correct answer? ").strip().lower()
+		if answer == 'a':
+			print("Incorrect")
+		elif answer == 'b':
+			print("Incorrect")
+		elif answer == 'c':
+			print("Incorrect")
+		elif answer == 'd':
+			print("Correct!")
+			break
+		else:
+			print("Please type in a letter from the list")
+			
+	print("-------------------")
+	
+	
+	
+main()
